a7df6bfec7e24e529b8041eac323321e50d4676b
NehamaMandelbaum <nehama1836@gmail.com>
2022-12-28 10:05:03 -0300

    feat: global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import {useState} from "react";
+import {Header} from "./components/Header";
+
+import "./global.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Olá Mundo, Nehama!</h1>
     </div>
   );
